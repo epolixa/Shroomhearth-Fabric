47546fde4f4663101db32d125535e07c9bb15ee7
epo <epolixa@gmail.com>
2020-12-07 11:15:15 -0500

    initial commit

---
+++ src/main/java/com/epolixa/bityard/Bityard.java
@@ -0,0 +1,17 @@
+package com.epolixa.bityard;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bityard implements ModInitializer {
+
+    public static final String MOD_ID = "bityard";
+
+    @Override
+    public void onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        // However, some things (like resources) may still be uninitialized.
+        // Proceed with mild caution.
+
+        System.out.println("Hello Fabric world! It's Bityard!");
+    }
+}
