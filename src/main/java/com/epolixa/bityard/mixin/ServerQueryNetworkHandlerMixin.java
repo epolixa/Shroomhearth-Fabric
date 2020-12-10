f93b8009f902ff290e3855b707a437bb840a764d
epo <epolixa@gmail.com>
2020-12-10 11:49:20 -0500

    override server motd

---
+++ src/main/java/com/epolixa/bityard/mixin/ServerQueryNetworkHandlerMixin.java
@@ -0,0 +1,38 @@
+package com.epolixa.bityard.mixin;
+
+import net.minecraft.network.packet.c2s.query.QueryRequestC2SPacket;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.network.ServerQueryNetworkHandler;
+import net.minecraft.text.Text;
+import net.minecraft.util.DyeColor;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(ServerQueryNetworkHandler.class)
+public class ServerQueryNetworkHandlerMixin {
+
+    @Shadow @Final private MinecraftServer server;
+
+    @Inject(method = "onRequest", at = @At("HEAD"))
+    public void onRequest(QueryRequestC2SPacket packet, CallbackInfo info) {
+        try {
+            System.out.println("[ServerQueryNetworkHandlerMixin][onRequest] enter");
+
+            String motdMessage = "Hello, epo!";
+            DyeColor motdColor = DyeColor.GREEN;
+            String motdJSON = "[{\"text\":\"" + motdMessage + "\"}]";
+
+            server.getServerMetadata().setDescription(Text.Serializer.fromJson(motdJSON));
+
+            System.out.println("[ServerQueryNetworkHandlerMixin][onRequest] exit");
+        } catch (Exception e) {
+            System.out.println("[ServerQueryNetworkHandlerMixin][onRequest] caught error: " + e);
+        }
+    }
+}
