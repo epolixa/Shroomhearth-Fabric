0b47c7e8415d11d209974ffb262b27379254cea2
epo <epolixa@gmail.com>
2020-12-17 09:19:10 -0500

    seperate default config values to mod init

--- src/main/java/com/epolixa/bityard/Bityard.java
+++ src/main/java/com/epolixa/bityard/Bityard.java
@@ -19,6 +19,7 @@ import java.util.*;
 public class Bityard implements ModInitializer {
 
     public static final String MOD_ID = "bityard";
+    public static final Map<String, Integer> defaultConfig = Maps.newHashMap();
 
     @Override
     public void onInitialize() {
@@ -27,6 +28,8 @@ public class Bityard implements ModInitializer {
 
             BityardUtils.log("registering server lifecycle events");
             ServerLifecycleEvents.SERVER_STARTED.register((server) -> onServerStarted(server));
+
+            initializeDefaultConfig();
 
             BityardUtils.log("exit");
         } catch (Exception e) {
@@ -40,6 +43,39 @@ public class Bityard implements ModInitializer {
 
             setMOTD(server);
             setupScoreboardConfig(server);
+
+            BityardUtils.log("exit");
+        } catch (Exception e) {
+            BityardUtils.log("caught error: " + e);
+        }
+    }
+
+
+    private void initializeDefaultConfig() {
+        try {
+            BityardUtils.log("enter");
+
+            // AFK idle time
+            defaultConfig.put("IDLE_TIME", 240);
+
+            // message of the day board area coords
+            defaultConfig.put("MOTD_X1", -346);
+            defaultConfig.put("MOTD_Y1", 67);
+            defaultConfig.put("MOTD_Z1", 259);
+            defaultConfig.put("MOTD_X2", -346);
+            defaultConfig.put("MOTD_Y2", 73);
+            defaultConfig.put("MOTD_Z2", 264);
+
+            // community gateway coords
+            defaultConfig.put("GATE_X", -378); // spawn gateway coords
+            defaultConfig.put("GATE_Y", 70);
+            defaultConfig.put("GATE_Z", 267);
+            defaultConfig.put("GATE_EXIT_X", -378); // spawn gateway exit coords
+            defaultConfig.put("GATE_EXIT_Y", 66);
+            defaultConfig.put("GATE_EXIT_Z", 261);
+            defaultConfig.put("RET_GATE_X", -6225); // return gateway coords
+            defaultConfig.put("RET_GATE_Y", 73);
+            defaultConfig.put("RET_GATE_Z", 1099);
 
             BityardUtils.log("exit");
         } catch (Exception e) {
@@ -112,31 +148,7 @@ public class Bityard implements ModInitializer {
             // capture the scoreboard
             ServerScoreboard scoreboard = server.getScoreboard();
 
-            // define hardcoded values
             String configPlayerName = MOD_ID;
-            Map<String, Integer> defaultConfig = Maps.newHashMap();
-
-            // AFK idle time
-            defaultConfig.put("IDLE_TIME", 240);
-
-            // message of the day board area coords
-            defaultConfig.put("MOTD_X1", -346);
-            defaultConfig.put("MOTD_Y1", 67);
-            defaultConfig.put("MOTD_Z1", 259);
-            defaultConfig.put("MOTD_X2", -346);
-            defaultConfig.put("MOTD_Y2", 73);
-            defaultConfig.put("MOTD_Z2", 264);
-
-            // community gateway coords
-            defaultConfig.put("GATE_X", -378); // spawn gateway coords
-            defaultConfig.put("GATE_Y", 70);
-            defaultConfig.put("GATE_Z", 267);
-            defaultConfig.put("GATE_EXIT_X", -378); // spawn gateway exit coords
-            defaultConfig.put("GATE_EXIT_Y", 66);
-            defaultConfig.put("GATE_EXIT_Z", 261);
-            defaultConfig.put("RET_GATE_X", -6225); // return gateway coords
-            defaultConfig.put("RET_GATE_Y", 73);
-            defaultConfig.put("RET_GATE_Z", 1099);
 
             // for each default value...
             for (Map.Entry<String, Integer> entry : defaultConfig.entrySet()) {
