ad0a4495943e3b7246b85c191b97bc10fd87d92d
epolixa <epolixa@gmail.com>
2023-06-01 15:24:54 -0400

    small chance for special item offer

--- src/main/java/com/epolixa/shroomhearth/mixin/WanderingTraderEntityMixin.java
+++ src/main/java/com/epolixa/shroomhearth/mixin/WanderingTraderEntityMixin.java
@@ -8,7 +8,9 @@ import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.item.*;
 import net.minecraft.potion.PotionUtil;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntryList;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
@@ -27,6 +29,7 @@ public abstract class WanderingTraderEntityMixin extends MerchantEntity {
 
     private static final TagKey<Item> BLACKLIST = TagKey.of(RegistryKeys.ITEM, new Identifier(Shroomhearth.MOD_ID, "trader_blacklist"));
     private static final TagKey<Item> POTIONS = TagKey.of(RegistryKeys.ITEM, new Identifier(Shroomhearth.MOD_ID, "potions"));
+    private static final TagKey<Item> SPECIALS = TagKey.of(RegistryKeys.ITEM, new Identifier(Shroomhearth.MOD_ID, "trader_specials"));
 
     public WanderingTraderEntityMixin(EntityType<? extends WanderingTraderEntity> entityType, World world) {
         super(entityType, world);
@@ -57,6 +60,11 @@ public abstract class WanderingTraderEntityMixin extends MerchantEntity {
                     pickedItems.add(rItem); // add item to picked list so we don't pick it again
                     tradeOfferList.add(buildTradeOffer(r, rItemStack)); // build an offer for the item
                 }
+            }
+
+            // chance to add a special offer
+            if (r.nextBoolean()) {
+                tradeOfferList.add(buildSpecialTradeOffer(r));
             }
 
             // mix up tradeOffers order
@@ -94,6 +102,34 @@ public abstract class WanderingTraderEntityMixin extends MerchantEntity {
             } else { // sell
                 tradeOffer = new TradeOffer(emeraldStack, itemStack, uses, ShroomhearthUtils.inRange(r, 3, 6), 0.2f);
             }
+
+        } catch (Exception e) {
+            Shroomhearth.LOG.error("Caught error: " + e);
+            e.printStackTrace();
+        }
+
+        return tradeOffer;
+    }
+
+
+    private TradeOffer buildSpecialTradeOffer(Random r) {
+        TradeOffer tradeOffer = null;
+
+        try {
+            // Pull a random item from trader_specials tag
+            Item item = Registries.ITEM.getEntryList(SPECIALS).flatMap(list -> list.getRandom(r)).get().value();
+            ItemStack itemStack = item.getDefaultStack();
+
+            // special case for potions
+            /*if (itemStack.isIn(POTIONS)) {
+                PotionUtil.setPotion(itemStack, Registries.POTION.get(r.nextInt(Registries.POTION.size())));
+            }*/
+
+            ItemStack emeraldStack = new ItemStack(Items.EMERALD.asItem());
+            emeraldStack.setCount(ShroomhearthUtils.inRange(r, 24, 40));
+
+            // small chance to be a buy offer instead of sell offer
+            tradeOffer = new TradeOffer(emeraldStack, itemStack, 1, ShroomhearthUtils.inRange(r, 3, 6), 0.2f);
 
         } catch (Exception e) {
             Shroomhearth.LOG.error("Caught error: " + e);
